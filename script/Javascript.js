1defa729f4506d80e333faf3e7645ae4d1c8ab4d
Gabriel <gabrielrrodriguesds@outlook.com>
2019-10-20 13:16:10 -0200

    Projeto Finalizado

---
+++ script/Javascript.js
@@ -0,0 +1,11 @@
+function mostra(id){
+			if (document.getElementById(id).style.display == 'block') {
+			document.getElementById(id).style.display = 'none';
+		} else {document.getElementById(id).style.display = 'block';}
+		}
+
+		function features(id){
+			if (document.getElementById(id).style.display == 'flex') {
+			document.getElementById(id).style.display = 'none';
+		} else {document.getElementById(id).style.display = 'flex';}
+		}
